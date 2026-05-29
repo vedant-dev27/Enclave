47c65c9b37a8caec84cbd2dfde934aeccfe1e64b
Vedant Singh <ani.vedant@outlook.com>
2026-05-29 16:40:22 +0530

    feat: display vault entries

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,10 +13,27 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    List<KdbxEntry> ls = widget.kdbxFile.body.rootGroup.entries.toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Enclave'),
         leading: Icon(Icons.menu),
+      ),
+      body: ListView.builder(
+        itemCount: ls.length,
+        itemBuilder: (context, index) {
+          final entry = ls[index];
+
+          return ListTile(
+            title: Text(
+              entry.getString(KdbxKeyCommon.TITLE)?.getText() ?? 'Untitled',
+            ),
+            subtitle: Text(
+              entry.getString(KdbxKeyCommon.PASSWORD)?.getText() ?? "Unknown",
+            ),
+          );
+        },
       ),
 
       floatingActionButton: FloatingActionButton(
